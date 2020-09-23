d7c02449e4f5669395509e787d44aa0e7cada604
xinjingwsz <xinjingwsz@163.com>
2020-09-23 11:05:30 +0800

    gotop html js ok

--- js/index.js
+++ js/index.js
@@ -1,34 +1,62 @@
-
-
-
-let routers = [
-	{
-		path:"/orderlist",
-		component:orderList
+let routers = [{
+		path: "/orderlist",
+		component: orderList
 	},
 	{
-		path:"/*",
-		component:orderList
+		path: "/*",
+		component: orderList
 	}
 ]
 let elemaRouter = new VueRouter({
-	routes:routers
+	routes: routers
 });
 
 
 //主导航组件
-let mainNavCpn =Vue.extend({
-	template:"#mainnavcpn"
+let mainNavCpn = Vue.extend({
+	template: "#mainnavcpn"
 });
-let mainnav = Vue.component('mainnav',mainNavCpn);
+let mainnav = Vue.component('mainnav', mainNavCpn);
+
+//回顶
+let goTopTemp = Vue.extend({
+	template: "#goTopTemp",
+	data: function() {
+		return {
+			topDistance:0
+		}
+	},
+	mounted() {
+		window.addEventListener("scroll", this.getTopDistance);
+	},
+	methods: {
+		getTopDistance() {
+			this.topDistance = document.documentElement.scrollTop || document.body.scrollTop || window.pageYOffset;
+		},
+		goTop() {
+			
+			let id = setInterval(function(){
+				let y = this.topDistance;
+				document.documentElement.scrollTop = y;
+				document.body.scrollTop = y;
+				window.pageYOffset = y;
+				if(y < 0){
+					clearInterval(id);
+				}
+			},50);
+		}
+	}
+});
+let gotopcpn = Vue.component('gotopcpn', goTopTemp);
+
 
 const index = new Vue({
-	el:"#index",
-	data:{
-		orderList:[]
+	el: "#index",
+	data: {
+		orderList: []
 	},
-	methods:{
+	methods: {
 
 	},
-	router:elemaRouter
+	router: elemaRouter
 });
