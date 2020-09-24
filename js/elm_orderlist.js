115dfc0276b31b83bc8bb164554ee59abff38e8a
xinjingwsz <xinjingwsz@163.com>
2020-09-24 10:13:33 +0800

    route ok

--- js/elm_orderlist.js
+++ js/elm_orderlist.js
@@ -1,32 +1,65 @@
 let orderList = {
-   template:"#orderlistcpn",
-   data:function(){
-       return{
-		   orderList:[],
-		   alreadyPayList:[],
-		   noPayList:[]
-	   }
-   },
-   methods:{
-        getOrderList(){//获取数据
-        	this.$http.get("./json/"+"orderlist" + ".json").then(function(res){
-        		this.orderList = res.body.dataZone.list;
-        		 this.alreadyPayList=[];
-        		 this.noPayList=[];
-        		for(let t of this.orderList){
-        		    if(t.stause == 1){
-        		        this.alreadyPayList.push(t);
-        		        console.log(t);
-        		    }else if(t.stause == 2){
-        				this.noPayList.push(t);
-        			}
-        		}
-        	},getDataFailed);
-        }
-    }
+	template: "#orderlistcpn",
+
+	data: function() {
+		return {
+			orderList: [],
+			alreadyPayList: [],
+			noPayList: [],
+			isOrderShow:false,
+			isOrderListShow:true,
+			orderInfo:{}
+		}
+	},
+	mounted() {
+		this.getOrderList();
+	},
+	methods: {
+		reback(){//从上个页面返回事件
+			console.log("orderlist.reback");
+			this.isOrderShow=false;
+			this.isOrderListShow=true;
+		},
+		goPay(ind){//跳转到支付页面  拿到索引 
+			console.log(this.noPayList[ind]);//获取索引中的数据
+			//将数据传输到子组件中
+			this.orderInfo= this.noPayList[ind];
+			//设置数据的订单页显示和隐藏
+			this.isOrderShow=true;
+			this.isOrderListShow=false;
+			console.log(this.isOrderShow);
+		},
+		getOrderList() { //获取数据
+			console.log("getorderlist");
+			this.$http.get("./json/" + "orderlist" + ".json").then(function(res) {
+				this.orderList = res.body.dataZone.list;
+				this.alreadyPayList = [];
+				this.noPayList = [];
+				for (let t of this.orderList) {
+					if (t.stause == 1) {
+						console.log(t.productlist);
+						t.amount = computeAmount(t.productlist) + t.deliveryCost;
+						this.alreadyPayList.push(t);
+					} else if (t.stause == 2) {
+						t.amount = computeAmount(t.productlist) + t.deliveryCost;
+						this.noPayList.push(t);
+					}
+				}
+			}, getDataFailed);
+		}
+	},
 };
 //链接失败
-function getDataFailed(){
-    console.log("failed");
-    //显示失败信息
+function getDataFailed() {
+	console.log("failed");
+	//显示失败信息
+}
+
+//计算总价
+function computeAmount(list){
+	sum = 0;
+	for (let item of list) {
+		sum += item.price * item.count;
+	}
+	return sum;
 }
--- js/index.js
+++ js/index.js
@@ -1,16 +1,35 @@
+//货币过滤器
+Vue.filter('moneyFmt', function(value) {
+	if (parseFloat(value)) {
+		return "￥" + parseFloat(value).toFixed(2);
+	}
+});
+
+//路由
 let routers = [{
 		path: "/orderlist",
 		component: orderList
 	},
 	{
+		path: "/discover",
+		component: discovertemp
+	},
+	{
+		path: "/main",
+		component: maintemp
+	},
+	{
+		path: "/mine",
+		component: minetemp
+	},
+	{
 		path: "/*",
-		component: orderList
+		component: maintemp
 	}
 ]
 let elemaRouter = new VueRouter({
 	routes: routers
 });
-
 
 //主导航组件
 let mainNavCpn = Vue.extend({
@@ -18,12 +37,26 @@ let mainNavCpn = Vue.extend({
 });
 let mainnav = Vue.component('mainnav', mainNavCpn);
 
+
+//顶部组件
+let toptemp = Vue.extend({
+	template: "#toptemp",
+	methods: {
+		back() {
+			console.log("back");
+			this.$emit("back", "1");
+		}
+	}
+});
+let topcpn = Vue.component('topcpn', toptemp);
+
+
 //回顶
 let goTopTemp = Vue.extend({
 	template: "#goTopTemp",
 	data: function() {
 		return {
-			topDistance:0
+			topDistance: 0
 		}
 	},
 	mounted() {
@@ -35,26 +68,29 @@ let goTopTemp = Vue.extend({
 			this.topDistance = document.documentElement.scrollTop || document.body.scrollTop || window.pageYOffset;
 		},
 		goTop() {
-			let id = setInterval(function(){
-				let y = $(window).scrollTop() ;
+			let id = setInterval(function() {
+				let y = $(window).scrollTop();
 				console.log(y);
-				if(y > 0){
-					y=y-10;
+				if (y > 0) {
+					y = y - 10;
 					$(window).scrollTop(y);
-				}else{
+				} else {
 					clearInterval(id);
 				}
-			},5);
+			}, 5);
 		}
 	}
 });
 let gotopcpn = Vue.component('gotopcpn', goTopTemp);
 
 
+
+
+//主组件
 const index = new Vue({
 	el: "#index",
 	data: {
-		orderList: []
+
 	},
 	methods: {
 
